a2beb1e2e762ebd1c523b126833fb939e4234184
dmitriypoletov <ddpoletov@edu.hse.ru>
2024-12-10 20:54:36 +0300

    First commit

--- QuiZZZ.py
+++ QuiZZZ.py
@@ -1,141 +1,120 @@
 import os
 import random
-from colorama import init
-from colorama import Fore, Back
+from unittest.mock import Mock
 
 
 def menu():
-	print()
-	print('Приветствую в QuiZZZ!')
-	print('Вот список доступных функций: (Введи одну или две буквы, чтобы выполнить соответствующую команду)')
-	print('> m - Вернуться в главное меню')
-	print('> st - Начать тестирование')
-	print('> sp - Остановить тестирование (работает только при проведении тестирования)')
-	print('> c - Выбрать сложность слов')
-	print('> t - Выбрать тематику слов')
-	print('> d - Повторить слова и их перевод')
-	print('> h - Помощь')
-	print('> e - Выйти из программы')
-	command = input()
-	commands = {'m': menu, 'st': start, 'c': complexity, 't': theme, 'd': dictionary, 'h': help_pls, 'e': exit}
-	while command not in commands:
-		command = input('Неизвестная команда, повтори ещё раз, пожалуйста: ')
-	print()
-	commands[command]()
+	yield 'Приветствую в QuiZZZ!\n\
+	Вот список доступных функций:\n\
+	> Вернуться в главное меню <\n\
+	> Настройки языка <\n\
+	> Начать тестирование <\n\
+	> Выбрать сложность слов <\n\
+	> Выбрать тематику слов <\n\
+	> Повторить слова и их перевод <\n\
+	> Помощь <\n\
+	> Выйти из программы <'
+	funcs = (menu, settings, start, stop, complexity, theme, dictionary, help_pls, exit)
+	func = Mock()
+	func()
 
 
-settings = 'английский - русский'
+def settings():
+	yield 'Выбери режим перевода:\n\
+	> Английский - Русский <\n\
+	> Русский - Английский <\n\
+	> Случайно <'
+	s = Mock()
+	return s
+
+
 def start():
-	global settings
-	change = input(f'Текущий режим перевода: {settings}. Поменять их местами? (Enter если нет, любой символ иначе)' )
+	setts = settings()
 	my_dict = {}
 	with open('words.txt', 'r', encoding = 'utf-8') as file:
 		words = file.readlines()
 		for item in words:
 			s = item.split(' - ')
-			if change:
+			if setts == 'Английский - Русский':
 				my_dict[s[0]] = s[-1].rstrip()
-				settings = 'русский - английский'
-			else:
+			elif setts == 'Русский - Английский':
 				my_dict[s[-1].rstrip()] = s[0]
+			else:
+				num = random.randint(0, 1)
+				if num == 0:
+					my_dict[s[0]] = s[-1].rstrip()
+				else:
+					my_dict[s[-1].rstrip()] = s[0]
 
 	dict_keys = list(my_dict)
 	dict_translations = list(my_dict.values())
 	score = 0
 	mistakes = []
 
-	print('Начинаем!')
+	yield 'Начинаем!'
 	for _ in range(len(dict_keys)):
 		word = random.choice(dict_keys)
 		s = dict_translations.copy()
 		variants = [0] * 4
-		right_answer = random.randint(0, 3)
-		variants[right_answer] = my_dict[word]
+		right_answer_index = random.randint(0, 3)
+		variants[right_answer_index] = my_dict[word]
 		s.remove(my_dict[word])
 		for i in range(4):
 			if variants[i] == 0:
 				variants[i] = s.pop(random.randint(0, len(s) - 1))
 		var1, var2, var3, var4 = variants
-		answer = input(f'Переведи слово: {word}. Варианты ответов: 1) {var1}; 2) {var2}; 3) {var3}; 4) {var4}. ')
-		while answer not in ('1', '2', '3', '4', 'sp'):
-			answer = input('Не понял тебя. Повтори, пожалуйста, ещё раз: (Подсказка: в ответе напиши одну из цифр 1, 2, 3 или 4, в зависимости от того, какая цифра стоит рядом с ответом, который ты считаешь правильным) ')
-		if answer == 'sp':
-			stop()
-			return
+		answer = Mock()		# Переведи слово: {word}. Варианты ответов: 1) {var1}; 2) {var2}; 3) {var3}; 4) {var4}.
 		answer = int(answer)
-		print(f'Правильный ответ: {Fore.BLACK}{Back.GREEN}{right_answer + 1}) {variants[right_answer]}{Fore.RESET}{Back.RESET}.')
-		if answer == right_answer + 1:
-			print(f'Твой ответ: {Fore.BLACK}{Back.GREEN}{answer}) {variants[answer - 1]}{Fore.RESET}{Back.RESET}.')
+		yield f'Правильный ответ: {right_answer_index + 1}) {variants[right_answer_index]}.'
+		if answer == right_answer_index + 1:
+			yield f'Твой ответ: {answer}) {variants[answer - 1]}.'
 			score += 1
 		else:
-			print(f'Твой ответ: {Fore.BLACK}{Back.RED}{answer}) {variants[answer - 1]}{Fore.RESET}{Back.RESET}.')
+			yield f'Твой ответ: {answer}) {variants[answer - 1]}.'
 			mistakes.append(word)
-		print()
 		dict_keys.remove(word)
-	print('Тест окончен!')
-	print(f'Правильных ответов: {score}/{len(my_dict)}.')
-	print()
-	ans = input('> m - Вернуться в главное меню ')
-	while ans != 'm':
-		ans = input('Неизвестная команда, повтори ещё раз, пожалуйста: ')
+	yield 'Тест окончен!'
+	yield f'Правильных ответов: {score}/{len(my_dict)}.'
 	menu()
 
 
 def stop():
-	print('Тестирование было прервано.')
-	print()
-	ans = input('> m - Вернуться в главное меню ')
-	while ans != 'm':
-		ans = input('Неизвестная команда, повтори ещё раз, пожалуйста: ')
+	yield 'Тестирование было остановлено. Уверен, что хочешь выйти?\n\
+	> Вернуться в главное меню <\n\
+	> Продолжить тест <'
 	menu()
 
 
 def complexity():
-	print('В разработке :)')
-	print()
-	ans = input('> m - Вернуться в главное меню ')
-	while ans != 'm':
-		ans = input('Неизвестная команда, повтори ещё раз, пожалуйста: ')
-	menu()
+	pass
 
 
 def theme():
-	print('В разработке :)')
-	print()
-	ans = input('> m - Вернуться в главное меню ')
-	while ans != 'm':
-		ans = input('Неизвестная команда, повтори ещё раз, пожалуйста: ')
-	menu()
+	pass
 
 
 def dictionary():
-	print('Вот твой текущий словарь.')
+	yield 'Вот твой текущий словарь.'
 	with open('words.txt', 'r', encoding = 'utf-8') as file:
 		words = file.readlines()
 		for item in words:
-			print(item.rstrip())
-	print()
-	ans = input('> m - Вернуться в главное меню ')
-	while ans != 'm':
-		ans = input('Неизвестная команда, повтори ещё раз, пожалуйста: ')
+			yield item.rstrip()
+	yield '> Вернуться в главное меню <'
 	menu()
 
 
 def help_pls():
 	try:
 		os.startfile('info.pdf')
-		print(f'Вся необходимая информация представлена в файле info.pdf.')
+		yield f'Вся необходимая информация представлена в файле info.pdf.'
 	except Exception as e:
-		print(f'Ошибка при открытии файла: {e}')
-	print()
-	ans = input('> m - Вернуться в главное меню ')
-	while ans != 'm':
-		ans = input('Неизвестная команда, повтори ещё раз, пожалуйста: ')
+		yield f'Ошибка при открытии файла: {e}'
+	yield '> Вернуться в главное меню <'
 	menu()
 
 
 def exit():
-	print('До новых встреч!')
+	return 'До новых встреч!'
 
-
-menu()
+if __name__ == "__main__":
+	menu()
