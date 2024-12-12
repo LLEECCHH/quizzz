59eed70a0652a4f6ee4ba7d5574041432d5bf8b8
Alex_Desktop <alexej.rutskij@gmail.com>
2024-12-12 18:32:57 +0300

    modified interface (not final)

--- interface.py
+++ interface.py
@@ -1,21 +1,51 @@
-#import QuiZZZ
-import PySimpleGUI as sg
-import random
+# import QuiZZZ
+import tkinter as tk
+from tkinter import ttk
 
-# что будет внутри окна
-# первым описываем кнопку и сразу указываем размер шрифта
-layout = [[sg.Button('Новое число',enable_events=True, key='-FUNCTION-', font='Helvetica 16')],
-        # затем делаем текст
-        [sg.Text('Результат:', size=(25, 1), key='-text-', font='Helvetica 16')]]
-# рисуем окно
-window = sg.Window('QuiZZZ', layout, size=(350,100))
-# запускаем основной бесконечный цикл
-while True:
-    # получаем события, произошедшие в окне
-    event, values = window.read()
-    # если нажали на крестик
-    if event in (sg.WIN_CLOSED, 'Exit'):
-        # выходим из цикла
-        break
-# закрываем окно и освобождаем используемые ресурсы
-window.close()
+deffont = 'Helvetica'
+btnfont = ('Helvetica', 5)
+
+
+def menu():
+    global menufrm
+    menufrm = tk.Frame(window)
+    menufrm.pack()
+    startlbl = ttk.Label(menufrm, text='Добро пожаловать', font=(deffont, 20))
+    startlbl.pack(side=tk.TOP)
+    startbtn = ttk.Button(menufrm, text='Начать тест', command=start)
+    startbtn.pack(side=tk.BOTTOM, pady=50, ipady=20, ipadx=20)
+
+
+def start():
+    global frm_top, frm_bottom
+    menufrm.destroy()
+    testfrm = tk.Frame()
+    testfrm.pack()
+    testlbl = ttk.Label(window, text="Выберите правильный ответ", font=(deffont, 20))
+    testlbl.place(relx=0.5, rely=0.1, anchor='center')
+
+    frm_btm = tk.Frame(window)
+    frm_btm.pack(side=tk.BOTTOM, anchor='center')
+
+    frm_top = tk.Frame(window)
+    frm_top.pack(side=tk.BOTTOM, anchor='center')
+
+    btn1 = ttk.Button(frm_top, text='Вариант 1')
+    btn1.pack(side=tk.LEFT, anchor='center', fill='x', ipadx=100, ipady=20)
+    
+    btn2 = ttk.Button(frm_top, text='Вариант 2')
+    btn2.pack(side=tk.LEFT, anchor='center', fill='x', ipadx=100, ipady=20)
+
+    btn3 = ttk.Button(frm_btm, text='Вариант 3')
+    btn3.pack(side=tk.LEFT, anchor='center', fill='x', ipadx=100, ipady=20)
+
+    btn4 = ttk.Button(frm_btm, text='Вариант 4')
+    btn4.pack(side=tk.LEFT, anchor='center', fill='x', ipadx=100, ipady=20) 
+
+window = tk.Tk()
+window.title("Quizzz")
+window.geometry('640x480')
+window.minsize(640,480)
+window.maxsize(640,480)
+menu()
+window.mainloop()
